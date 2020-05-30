7c71a2786710213fb49e93dee97d0487702c53f5
Benjamin Kinga <somethingscripted@gmail.com>
2020-05-30 14:27:09 -0500

    updating

--- src/index.js
+++ src/index.js
@@ -13,27 +13,15 @@ const resolvers = {
         url: args.url,
         description: args.description
       })
-    },
-
-    updateLink: (parent, args) => {
-      const item = links.find(link => link.id === args.id);
-      if (args.url) item.url = args.url;
-      if (args.description) item.description = args.description;
-      return item
     }
   },
-
-  deleteLink: (parent, args) => {
-    const item = links.find(link => link.id === args.id);
-  }
 
   Link: {
     id: (parent) => parent.id,
     description: (parent) => parent.description,
     url: (parent) => parent.url,
   },
-
-}
+};
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
